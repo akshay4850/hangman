ff0a179d211ad0bbb5de8d9e8cb775c3969149d0
akshay4850 <83793809+akshay4850@users.noreply.github.com>
2022-09-13 23:11:22 +1000

    Delete HangMan directory

--- HangMan/dictio.h
+++
@@ -1,4 +0,0 @@
-#define T_MAX_WORD 100
-
-void pickWord(char*);
-int randInt(const int);
--- HangMan/game.h
+++
@@ -1,7 +0,0 @@
-void run(int);
-char readChar();
-int win(const int*, const int);
-int suggestLetter(const char*, int*, const int, const char);
-
-int len(const char*);
-void initTab(int*, const int);
